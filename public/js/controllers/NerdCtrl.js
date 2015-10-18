7e88c3c57c87275fb296d4a592e4f9ffc14c65b3
unknown <ferstlk@MSGN00013.int.root.msg.ag>
2015-10-18 23:15:42 +0200

    functionality + style

--- public/js/controllers/NerdCtrl.js
+++ public/js/controllers/NerdCtrl.js
@@ -3,11 +3,51 @@
  */
 
 angular.module('NerdCtrl', []).controller('NerdController',
-    function($scope, Nerd) {
+    function($scope, Nerd, $timeout) {
+
+        $scope.newNerd = "";
 
         $scope.tagline = 'Nothing beats a pocket protector!';
         Nerd.get().then(function(res) {
             $scope.nerds = res.data;
         });
+
+        $scope.deleteNerd = function(nerd) {
+            Nerd.delete(nerd._id).then(function() {
+                Nerd.get().then(function(res) {
+                    $scope.nerds = res.data;
+                    $scope.message = "Successfully deleted nerd!";
+                    $timeout(function() {
+                        $scope.message = "";
+                    }, 1200);
+                });
+            }, function(err) {
+                $scope.errMessage="An Error occured...";
+                $timeout(function() {
+                    $scope.errMessage = "";
+                }, 1200);
+            });
+        };
+
+        $scope.addNerd = function(name) {
+            if(name) {
+                Nerd.create({"name":name}).then(function() {
+                    // alert("Nerd created");
+                    Nerd.get().then(function(res) {
+                        $scope.nerds = res.data;
+                    });
+                    $scope.newNerd = "";
+                    $scope.message = "Successfully added nerd!";
+                    $timeout(function() {
+                        $scope.message = "";
+                    }, 1200)
+                }, function(err) {
+                    $scope.errMessage="An Error occured...";
+                    $timeout(function() {
+                        $scope.errMessage = "";
+                    }, 1200);
+                });
+            }
+        };
 
 });
